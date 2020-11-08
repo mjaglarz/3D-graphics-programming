ed5a106b1dfd52bfa9718124530f51b9d3a0ef00
Michał Jaglarz <mjaglarz98@gmail.com>
2020-11-08 10:59:23 +0100

    Copied directory and changed name to Uniforms.

---
+++ src/Exercises/Uniforms/app.cpp
@@ -0,0 +1,80 @@
+//
+// Created by pbialas on 25.09.2020.
+//
+
+#include "app.h"
+
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+
+#include "Application/utils.h"
+
+void SimpleShapeApplication::init() {
+
+
+    auto program = xe::create_program(std::string(PROJECT_DIR) + "/shaders/base_vs.glsl",
+                                      std::string(PROJECT_DIR) + "/shaders/base_fs.glsl");
+
+
+    if (!program) {
+        std::cerr << "Cannot create program from " << std::string(PROJECT_DIR) + "/shaders/base_vs.glsl" << " and ";
+        std::cerr << std::string(PROJECT_DIR) + "/shaders/base_fs.glsl" << " shader files" << std::endl;
+    }
+
+    std::vector<GLfloat> vertices = {
+            -0.6f, 0.3f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.0f, 0.9f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.6f, 0.3f, 0.0f, 1.0f, 0.0f, 0.0f,
+            0.4f, 0.3f, 0.0f, 0.0f, 0.0f, 1.0f,
+            0.4f, -0.3f, 0.0f, 0.0f, 0.0f, 1.0f,
+            -0.4f, 0.3f, 0.0f, 0.0f, 0.0f, 1.0f,
+            -0.4f, -0.3f, 0.0f, 0.0f, 0.0f, 1.0f
+    };
+
+    GLuint v_buffer_handle;
+    glGenBuffers(1, &v_buffer_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    std::vector<GLushort> indices = {
+            0, 1, 2,
+            3, 4, 5,
+            4, 6, 5
+    };
+
+    GLuint idx_buffer_handle;
+    glGenBuffers(1, &idx_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLushort), indices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    glGenVertexArrays(1, &vao_);
+    glBindVertexArray(vao_);
+    glBindBuffer(GL_ARRAY_BUFFER, v_buffer_handle);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buffer_handle);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(0));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), reinterpret_cast<GLvoid *>(3*sizeof(GLfloat)));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+    glClearColor(0.81f, 0.81f, 0.8f, 1.0f);
+    int w, h;
+    std::tie(w, h) = frame_buffer_size();
+    glViewport(0, 0, w, h);
+
+    glEnable(GL_DEPTH_TEST);
+    glUseProgram(program);
+}
+
+void SimpleShapeApplication::frame() {
+    glBindVertexArray(vao_);
+    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid *>(0));
+
+    glBindVertexArray(0);
+}
