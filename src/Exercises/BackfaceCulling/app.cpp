280ed2df1513121d34473badfe479a43de3cd29b
Michał Jaglarz <mjaglarz98@gmail.com>
2020-11-21 15:58:36 +0100

    Used backface culling.

--- src/Exercises/BackfaceCulling/app.cpp
+++ src/Exercises/BackfaceCulling/app.cpp
@@ -59,12 +59,12 @@ void SimpleShapeApplication::init() {
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     std::vector<GLushort> indices = {
-            0, 1, 2,
+            2, 1, 0,
             3, 4, 5,
-            6, 7, 8,
+            8, 7, 6,
             9, 10, 11,
             12, 13, 14,
-            13, 14, 15
+            15, 14, 13
     };
 
     GLuint idx_buffer_handle;
@@ -133,6 +133,10 @@ void SimpleShapeApplication::init() {
     } else {
         glUniformBlockBinding(program, u_transformations_index, 1);
     }
+
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glCullFace(GL_BACK);
 }
 
 void SimpleShapeApplication::frame() {
