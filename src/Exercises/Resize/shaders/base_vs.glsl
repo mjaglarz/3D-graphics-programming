128324bb7ed883fe39e995c244dc2173cd931d18
Michał Jaglarz <mjaglarz98@gmail.com>
2020-11-21 13:29:31 +0100

    Copied directory and changed name to Resize.

---
+++ src/Exercises/Resize/shaders/base_vs.glsl
@@ -0,0 +1,17 @@
+#version 410
+
+layout(location=0) in vec4 a_vertex_position;
+layout(location=1) in vec3 a_vertex_color;
+
+layout(std140) uniform Transformations {
+    mat4 P;
+    mat4 V;
+    mat4 M;
+};
+
+out vec3 vertex_color;
+
+void main() {
+    gl_Position = P * V * M * a_vertex_position;
+    vertex_color = a_vertex_color;
+}
